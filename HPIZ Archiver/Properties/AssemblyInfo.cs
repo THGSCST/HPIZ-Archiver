e10e1b0af95562893e5da147a68f9fcc46468a5d
THGSCST <thiagodesacosta@gmail.com>
2026-07-04 23:26:21 +0800

    Switch project to AnyCPU and update metadata

--- HPIZ Archiver/Properties/AssemblyInfo.cs
+++ HPIZ Archiver/Properties/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("HPIZ Archiver")]
-[assembly: AssemblyDescription("Total Annihilation HPI Zopfli Archiver")]
+[assembly: AssemblyTitle("HPIZ Archiver - Total Annihilation HPI Tool")]
+[assembly: AssemblyDescription("View, extract, merge, repack, and compress Total Annihilation HPI archives.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Total Annihilation HPI Zopfli Archiver")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("THGSCST")]
+[assembly: AssemblyProduct("HPIZ Archiver")]
+[assembly: AssemblyCopyright("Copyleft © THGSCST 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
